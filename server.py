df10151a3f4c6311678da028a50d2cd6a83931eb
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-03 17:18:54 -0400

    Make initial server

--- server.py
+++ server.py
@@ -1,8 +1,26 @@
 import socket
-from gpiozero import LED
+
+# from gpiozero import LED
 from dotenv import load_dotenv
+import os
+import log
 
 load_dotenv()
 
-led = LED(17)
+# led = LED(17)
+port = int(os.getenv("PORT"))
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((socket.gethostname(), port))
+    log.info(f"Binded to port {port}")
+except Exception as e:
+    log.danger(f"Failed to bind to port {port}, {e}")
+
+s.listen(5)
+log.good("Listening...")
+
+while True:
+    clientsocket, address = s.accept()
+    log.info(f"Connection from {address[0]} has been established.")
